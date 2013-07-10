d286f4dea7d87213e68cdd0d65525c5fd3b4cee3
Lokesh Tuteja <lokesh.tuteja@yahoo.co.in>
2013-07-11 00:05:10 +0530

    Update problem_001.py

--- project_euler/problem_001.py
+++ project_euler/problem_001.py
@@ -1,7 +1,6 @@
 
-
 def ismultiple3or5(i):
-	"This functino returns true if a number is a multiple of 3 or 5"
+	"This function returns true if a number is a multiple of 3 or 5"
 	if (i % 5 == 0 or i % 3 == 0):
 		return True
 	else:
