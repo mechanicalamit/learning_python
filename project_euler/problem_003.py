308b19e8ead6864e63ad17c4f4e5f7182bb130fd
Lokesh Tuteja <lokesh.tuteja@yahoo.co.in>
2013-07-13 00:08:58 +0530

    Create problem_003.py
    program does not give desired result

---
+++ project_euler/problem_003.py
@@ -0,0 +1,15 @@
+print('this program displays the prime factors of a number entered')
+i=1
+j=1
+k=0
+num=int(input('enter a number'))
+while i<=num:
+  if num%i==0:
+    k=0
+    while j<=i
+      if i%j==0:
+        k=k+1
+      j=j+1
+      if k==2:
+        print(i)
+  i=i+1      
