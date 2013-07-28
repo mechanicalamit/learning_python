cec46fb0f60ae78dabb1f3a9a584cc85c9c6d06a
mechanicalamit <mechanicalamit@gmail.com>
2013-07-28 11:58:13 -0500

    problem_003.py : Added hints to make better
    Added certain hints to make the code better.
    
    Don't worry about rewriting the complete program even if it is working. That is one of the things about software, you have to quite commonly rewrite complete programs or functions.

--- project_euler/problem_003.py
+++ project_euler/problem_003.py
@@ -1,3 +1,24 @@
+""" 
+Project Euler Problem #3
+
+Largest prime factor
+Problem 3
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
+
+""" Hints to make better
+1. Make a function isprime(int) which returns True of False
+2. Instead of counting up from 1 to search for largest prime factor,
+   you can count down from the number itselt. The first match is the
+   largest, no need to store a max
+3. It can be optimized further too, hint will be updated, once 1 and 2 
+   are completed.
+"""
+
+
 print('this program displays the prime factors of a number entered')
 i=1
 j=1
