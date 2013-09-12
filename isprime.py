6f96dc077e3dc54bfa2006ce6cecfc4d67940cca
Lokesh Tuteja <lokesh.tuteja@yahoo.co.in>
2013-09-13 01:04:17 +0530

    Update isprime.py

--- isprime.py
+++ isprime.py
@@ -1,23 +1,22 @@
 #to check whether  a number is prime
 from math import sqrt
-n=0;
 x=0;
-def isprime():
+def isprime(n):
     i=2;
     count=0;
     x=int(sqrt(n))
-    while i<=x:
+    for i in range(2,x+1):
         if n%i==0:
             count=count+1
-            i=i+1
-        else:
-            i=i+1
+            if count==1:
+                print('False')
+                return
     if count==0:
-        print(' True')
+        print('True')
     else:
-        print('false')
+        print('False')
 n=int(input('Enter a number :'))
-isprime()
+isprime(n)
 
 """
 Lets write an algorithm to check for a prime
